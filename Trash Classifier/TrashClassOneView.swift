e8fa317780a6ea0569a471f5f807b8812f2cef72
Yuvan Subbiah <yuvansubbiah2010@gmail.com>
2024-01-25 21:58:13 -0800

    TrashClassOneView changes

--- Trash Classifier/TrashClassOneView.swift
+++ Trash Classifier/TrashClassOneView.swift
@@ -72,12 +72,26 @@ struct ScreenTwoView: View {
 }
 
 struct ScreenThreeView: View {
+    @State var isPickerShowing = false
+    @State var selectedImage: UIImage?
+    
     var body: some View {
-        ZStack {
-            VStack{
-                Text("Camera API")
+        VStack{
+            if selectedImage != nil {
+                Image(uiImage: selectedImage!)
+                    .resizable()
+                    .frame(width:200,height:200)
+            }
+            Button {
+                isPickerShowing = true
+            } label: {
+                Text("Select a Photo")
             }
         }
+        .sheet(isPresented: $isPickerShowing, onDismiss: nil, content: {
+            // Image picker
+            ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+        })
     }
 }
 #Preview {
